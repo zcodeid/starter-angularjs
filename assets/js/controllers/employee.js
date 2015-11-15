6ef94d4e7c65346c62302ada7c05d9e90e65b8fa
Prawito <prawitohudoro@gmail.com>
2015-11-15 17:59:52 +0700

    form push data

--- assets/js/controllers/employee.js
+++ assets/js/controllers/employee.js
@@ -5,8 +5,13 @@ app.controller('EmployeeCtrl', function($scope, EmployeeSvc) {
 app.controller('EmployeeFormCtrl', function($scope, $state, $stateParams, EmployeeSvc) {
 	$scope.title = $stateParams.employee == null ? 'Add Employee' : 'Edit Employee';
     $scope.isEdit = $stateParams.employee != null;
+    $scope.employeeData = [];
 
     $scope.save = function(){
-    	$state.go('employee');
+    	// $state.go('employee');
+    	$scope.employeeData.push($scope.form);
+    	console.log($scope.employeeData);
     }
+
+
 });
